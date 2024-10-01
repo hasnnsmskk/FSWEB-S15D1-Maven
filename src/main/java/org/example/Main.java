0206794dc0b7e4deae523d01fff1adb3aef130ec
Hasan Şimşek <168709291+hasnnsmskk@users.noreply.github.com>
2024-10-01 22:34:07 +0300

    S15D1 completed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        /*Grocery grocery = new Grocery();
+        grocery.startGrocery();*/
 
+        /*MobilePhone mobilePhone = new MobilePhone("23156465230", new ArrayList<>());
+        mobilePhone.addNewContact(new Contact("hasan", "5366569876"));
+        mobilePhone.addNewContact(new Contact("hasan", "5366569876"));
+        mobilePhone.addNewContact(new Contact("asdad", "65515655"));
+        mobilePhone.printContacts();*/
     }
 }
